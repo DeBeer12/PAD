31d71baf43c6c0432ab9f6e78467167ceb87fb96
Safak <safak.inan61@hotmail.com>
2019-04-24 15:20:39 +0200

    Trying the timer class

--- src/sample/controllers/MainController.java
+++ src/sample/controllers/MainController.java
@@ -11,13 +11,15 @@ import sample.models.Game;
 
 public class MainController {
 
-    DBController controller = new DBController();
+    private DBController controller = new DBController();
     private int gat1;
     private int gat2;
     private int gat3;
     private int gat4;
     private int gat5;
     private int ballen = 10;
+
+
 
     public void initialize() {
         System.out.println("Lets get the bread");
@@ -43,15 +45,27 @@ public class MainController {
         return;
         }
 
+
+    }
+
+    public static int returnPunten(){
         Timer labelUpdate = new Timer();
+        final int[] punten = new int[1];
         labelUpdate.schedule(new TimerTask() {
             @Override
             public void run() {
-                score.setText("Score: " + punten);
-                balls.setText("Ballen: " + ballen);
+                Game game = new DBController().getGame();
+                int gat1 = game.getSensor1();
+                int gat2 = game.getSensor2();
+                int gat3 = game.getSensor3();
+                int gat4 = game.getSensor4();
+                int gat5 = game.getSensor5();
+
+                punten[0] = (gat1 * 100) + (gat2 * 100) + (gat3 * 50) + (gat4 * 25) + (gat5 * 0);
             }
         }, 10);
 
+        return punten[0];
     }
 
     public void resetGame(Button button) {
