25bf725801706204e97d29b928545d89ef265563
Safak <safak.inan61@hotmail.com>
2019-05-01 21:52:01 +0200

    Fixed live scoreboard (still has bugs, needs lookup)

--- src/sample/controllers/MainController.java
+++ src/sample/controllers/MainController.java
@@ -1,9 +1,11 @@
 package sample.controllers;
 
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Platform;
+import javafx.util.Duration;
 import sample.data.DBController;
-import java.util.Timer;
-import java.util.TimerTask;
 
 
 import javafx.scene.control.*;
@@ -17,16 +19,21 @@ public class MainController {
     private int gat3;
     private int gat4;
     private int gat5;
-    private int ballen = 10;
-
-
 
     public void initialize() {
         System.out.println("Lets get the bread");
-        //controller = new DBController();
     }
 
     public void setScore(Label score, Label balls) {
+            Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(1), ev -> {
+                score.setText("Score: "+ returnPunten());
+                balls.setText("Ballen: " + returnBallen());
+            }));
+            timeline.setCycleCount(Animation.INDEFINITE);
+            timeline.play();
+    }
+
+    private int returnPunten(){
         Game game = controller.getGame();
         gat1 = game.getSensor1();
         gat2 = game.getSensor2();
@@ -35,37 +42,19 @@ public class MainController {
         gat5 = game.getSensor5();
 
         int punten = (gat1 * 100) + (gat2 * 100) + (gat3 * 50) + (gat4 * 25) + (gat5 * 0);
-        ballen -= (gat1 + gat2 + gat3 + gat4 + gat5);
-
-        score.setText("Score: " + punten);
-        balls.setText("Ballen: " + ballen);
-
-        if (ballen == 0) {
-            System.out.println("Finished");
-        return;
-        }
-
-
+        return punten;
     }
 
-    public static int returnPunten(){
-        Timer labelUpdate = new Timer();
-        final int[] punten = new int[1];
-        labelUpdate.schedule(new TimerTask() {
-            @Override
-            public void run() {
-                Game game = new DBController().getGame();
-                int gat1 = game.getSensor1();
-                int gat2 = game.getSensor2();
-                int gat3 = game.getSensor3();
-                int gat4 = game.getSensor4();
-                int gat5 = game.getSensor5();
+    private int returnBallen(){
+        Game game = controller.getGame();
+        gat1 = game.getSensor1();
+        gat2 = game.getSensor2();
+        gat3 = game.getSensor3();
+        gat4 = game.getSensor4();
+        gat5 = game.getSensor5();
 
-                punten[0] = (gat1 * 100) + (gat2 * 100) + (gat3 * 50) + (gat4 * 25) + (gat5 * 0);
-            }
-        }, 10);
-
-        return punten[0];
+        int ballen = 10 - (gat1 + gat2 + gat3 + gat4 + gat5);
+        return ballen;
     }
 
     public void resetGame(Button button) {
@@ -75,7 +64,4 @@ public class MainController {
         });
     }
 
-    public int getBallen() {
-        return ballen;
-    }
 }
